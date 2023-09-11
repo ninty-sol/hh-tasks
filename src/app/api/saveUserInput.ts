a93925b7678505be2eeb65f686c2ac11e6106814
vivekpal1 <vivekpal@duck.com>
2023-09-11 19:03:03 +0530

    input.json

--- src/app/api/saveUserInput.ts
+++ src/app/api/saveUserInput.ts
@@ -14,4 +14,14 @@ export default async function handler(
         twitter,
       };
 
-      
+      await fs.writeFile('path/to/userInput.json', JSON.stringify(userInput));
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error('Error saving user input:', error);
+      res.status(500).json({ error: 'Unable to save user input' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
